e1236ab7bf3202518a25eaa9fcf7e74f6e99144e
David Aviles Leiva <daviles-@c2r10s2.42madrid.com>
2023-10-19 19:06:31 +0200

    Fix leaks on childs

--- srcs/childs.c
+++ srcs/childs.c
@@ -6,7 +6,7 @@
 /*   By: daviles- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/07 02:25:36 by daviles-          #+#    #+#             */
-/*   Updated: 2023/10/13 03:09:05 by daviles-         ###   ########.fr       */
+/*   Updated: 2023/10/19 19:06:10 by daviles-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "../include/pipex.h"
@@ -18,12 +18,12 @@ void	child_input(int *m_fd, char **av, char **env)
 	char	*cmd;
 	char	**cmds;
 
-	cmd = getcmd_withpath(av[2], &cmds, env);
 	pid = fork();
 	if (pid < 0)
 		ft_perror_exit("Fork error");
 	else if (pid == 0)
 	{
+		cmd = getcmd_withpath(av[2], &cmds, env);
 		fd_in = open(av[1], O_RDONLY);
 		if (fd_in < 0)
 			ft_perror_exit(av[1]);
@@ -44,12 +44,12 @@ pid_t	child_output(int *m_fd, char **av, char **env)
 	char	*cmd;
 	char	**cmds;
 
-	cmd = getcmd_withpath(av[3], &cmds, env);
 	pid = fork();
 	if (pid < 0)
 		ft_perror_exit("Fork error");
 	else if (pid == 0)
 	{
+		cmd = getcmd_withpath(av[3], &cmds, env);
 		fd_out = open(av[4], O_WRONLY | O_CREAT | O_TRUNC, 0666);
 		if (fd_out < 0)
 			ft_perror_exit(av[4]);
